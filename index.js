8e3ee6dbeab5b5b08d152c766249f16890b62790
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-05-26 13:05:03 +0530

    my-orders-cancel-button-done

--- index.js
+++ index.js
@@ -88,6 +88,13 @@ async function run(){
     res.send(cursor);
 })
 
+app.delete('/order/:id', async (req, res)=>{
+  const id = req.params.id;
+  const query = {_id: ObjectId(id)};
+  const result = await orderCollection.deleteOne(query);
+  res.send(result);
+})
+
     }
     finally{
 
